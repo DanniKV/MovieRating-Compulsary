7542a1fde42a1a021b1a0a5d6b551767954d2116
DanniKV <dannikvase@hotmail.com>
2018-11-01 14:51:17 +0100

    Initial commit

---
+++ MovieRating-Compulsary/MovieRatingEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieRating_Compulsary
+{
+    class MovieRatingEntity
+    {
+        public int Reviewer { get; set; }
+        public int Movie { get; set; }
+        public int Grade { get; set; }
+        public DateTime Date { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Reviewer: {0}\nMovie: {1}\nGrade: {2}\nDate: {3}\n", Reviewer, Movie, Grade, Date);
+        }
+    }
+}
